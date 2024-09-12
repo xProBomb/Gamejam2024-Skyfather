c9eed885c32f7c08c14ce7c783fb6e3d5d6b45a6
xProBomb <Justbomb03@gmail.com>
2024-09-11 20:27:25 -0700

    navmesh

--- Skyfather - Gamejam/Assets/Scripts/EnemyMovement.cs
+++ Skyfather - Gamejam/Assets/Scripts/EnemyMovement.cs
@@ -1,66 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField]
-    private float _enemySpeed = 1.0f;
-
-    [SerializeField]
-    private float _rotationSpeed = 1.0f;
-
-    private Rigidbody2D _enemyRigidbody;
-    private enemyAIController _enemyAIController;
-
-    private Vector2 _moveDirection;
+    private Vector3 target;
+    NavMeshAgent agent;
 
     void Awake()
     {
-        _enemyRigidbody = GetComponent<Rigidbody2D>();
-        _enemyAIController = GetComponent<enemyAIController>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateUpAxis = false;
+        agent.updateRotation = false;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        UpdateDirection();
-        FaceTarget();
-        Move();
-    }
-    
-    private void UpdateDirection()
-    {
-        if (_enemyAIController.AwareoOfPlayer)
-        {
-            _moveDirection = _enemyAIController.DirectionToPlayer;
-        }
-        else
-        {
-            _moveDirection = Vector2.zero;
-        }
+        target = GameObject.Find("Player").transform.position;
+        agent.SetDestination(target);
     }
 
-    private void FaceTarget()
-    {
-        if (_moveDirection != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(_moveDirection.y, _moveDirection.x) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-        }
-    }
 
-    private void Move()
-    {
-        if (_moveDirection != Vector2.zero)
-        {
-            _enemyRigidbody.velocity = _moveDirection * _enemySpeed;
-        }
-        else
-        {
-            _enemyRigidbody.velocity = Vector2.zero;
-        }
-    }
 
 }
