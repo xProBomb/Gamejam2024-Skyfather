f6bec8c3e3c24fe8dacbae83206bee8ba8a1225d
ColinVasas <colinvasas@gmail.com>
2024-09-13 01:12:46 -0700

    the cards are basically working

--- Skyfather - Gamejam/Assets/Scripts/Modifier.cs
+++ Skyfather - Gamejam/Assets/Scripts/Modifier.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Modifier : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum BulletType
     {
-        
+        normal,
+        fire,
+        poison
     }
+    //public GameObject[] cards;
+    public List<GameObject> cards = new List<GameObject>();
+    public GameObject[] bulletPrefabs;
+    public float numShots = 1;
+    public BulletType bulletType = BulletType.normal;
+    public float speed = 8;
+    public GameObject cardHand;
+    public CardMod cardMod;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        // int i = 0;
+        // while(cardHand.transform.GetChild(i).childCount != 0 || i>10)
+        // {
+        //     Debug.Log(cardHand.transform.GetChild(i).gameObject.name);
+        //     cards.Add(cardHand.transform.GetChild(i).gameObject);
+        //     i++;
+        // }
+        // cardMod.cards
     }
 }
