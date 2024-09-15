6100e82778ee6d202b50988fb8c2569896853b34
xProBomb <Justbomb03@gmail.com>
2024-09-15 02:01:42 -0700

    Update GridManager.cs

--- Skyfather - Gamejam/Assets/Scripts/GridManager.cs
+++ Skyfather - Gamejam/Assets/Scripts/GridManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using NavMeshPlus.Components;
-using TMPro.EditorUtilities;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
